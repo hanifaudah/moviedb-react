e39e94ef9438cead42aff2db79f4b68ff8e6551e
hanifaudah <hanifarkanaudah@gmail.com>
2019-12-24 16:59:48 +0700

    Updates

---
+++ precache-manifest.92b42f5b76d52bd8dbe123d75438f3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d00eb23b1dd2f799614fb39ee5a4c5",
+    "url": "/moviedb-react/index.html"
+  },
+  {
+    "revision": "00d8724fcfadb7ebb416",
+    "url": "/moviedb-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2a09220f0a50d0bf76eb",
+    "url": "/moviedb-react/static/css/main.7de8b8f9.chunk.css"
+  },
+  {
+    "revision": "00d8724fcfadb7ebb416",
+    "url": "/moviedb-react/static/js/2.a060c9c6.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/moviedb-react/static/js/2.a060c9c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a09220f0a50d0bf76eb",
+    "url": "/moviedb-react/static/js/main.71b9b931.chunk.js"
+  },
+  {
+    "revision": "a8738899db1befe6fc42",
+    "url": "/moviedb-react/static/js/runtime-main.f0d639f9.js"
+  }
+]);
